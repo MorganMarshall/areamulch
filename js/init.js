58f30c608225b27eb55652d89101e45750974500
Morgan Marshall <28496781+MorganMarshall@users.noreply.github.com>
2017-12-19 21:47:07 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
   $('#slider').load('components/slider.html');
   $('#middleicons').load('components/middleicons.html')
  $('#contact').load('components/contact.html')
-  $('#calculator').load('components/calculator.html')
+  
  $('#footer').load('components/footer.html')
-
+$('#calculator').load('components/calculator.html')
 
 
 
